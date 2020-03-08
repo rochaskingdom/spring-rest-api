b726e1c94cbee66f2a1a8392e5543d5637724b30
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-03-08 13:33:06 -0300

    Cross Origin - Configuracao em controller especifico

--- src/main/java/com/spring/rest/api/contoller/UsuarioController.java
+++ src/main/java/com/spring/rest/api/contoller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController //Arquitetura REST
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
