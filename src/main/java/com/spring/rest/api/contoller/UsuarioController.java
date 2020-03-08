f18bd1ae633a3daf2b962ed280fd08dacc0a2096
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-03-07 23:22:52 -0300

    Criando no servico POST de Cadastro

--- src/main/java/com/spring/rest/api/contoller/UsuarioController.java
+++ src/main/java/com/spring/rest/api/contoller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.spring.rest.api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,18 +17,18 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    //Servico RESTfull
+    //  Servico RESTfull
     @GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
     public ResponseEntity<Usuario> relatorio(@PathVariable(value = "id") Long id,
                                              @PathVariable(value = "venda") Long venda) {
 
         Optional<Usuario> usuario = usuarioRepository.findById(id);
 
-        //retorno seria um relatorio
+        // retorno seria um relatorio
         return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
     }
 
-    //Servico RESTfull
+    // Servico RESTfull
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Usuario> listId(@PathVariable(value = "id") Long id) {
 
@@ -46,5 +43,23 @@ public class UsuarioController {
         List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 
         return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+        return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+    public ResponseEntity<Usuario> cadastrarVenda(@PathVariable Long iduser,
+                                                  @PathVariable Long idvenda) {
+
+        // Aqui seria o processo de venda
+        // Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+        return new ResponseEntity("id user: " + iduser + "idvenda: " + idvenda, HttpStatus.CREATED);
     }
 }
